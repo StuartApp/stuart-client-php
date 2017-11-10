c07e7da0ba30fae010962dc2e33da0f6deea2d2c
Maximilien Tyc <maximilien.tyc@gmail.com>
2017-11-10 10:40:46 +0100

    remove pricing feature commit

--- src/Stuart/Client.php
+++ src/Stuart/Client.php
@@ -4,29 +4,21 @@ namespace Stuart;
 
 use Stuart\Infrastructure\HttpClient;
 use Stuart\Repository\JobRepository;
-use Stuart\Repository\JobPricingRepository;
 
 class Client
 {
     private $jobRepository;
-    private $jobPricingRepository;
 
     public function __construct($authenticator)
     {
         $guzzleClient = new \GuzzleHttp\Client();
         $httpClient = new HttpClient($authenticator, $guzzleClient);
         $this->jobRepository = new JobRepository($httpClient);
-        $this->jobPricingRepository = new JobPricingRepository($httpClient);
     }
 
     public function createJob($job)
     {
         return $this->jobRepository->save($job);
-    }
-
-    public function getPricing($job)
-    {
-        return $this->jobPricingRepository->save($job);
     }
 
     public function getJob($jobId)
