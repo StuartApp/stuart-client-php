20f64b0c4244bc194c066f7b7012e5c54484049a
Maximilien Tyc <maximilien.tyc@gmail.com>
2018-05-24 14:18:54 +0200

    optimise and solution api endpoint use

--- src/Stuart/Routing/GraphHopper.php
+++ src/Stuart/Routing/GraphHopper.php
@@ -2,8 +2,28 @@
 
 namespace Stuart\Routing;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use Stuart\Infrastructure\ApiResponseFactory;
+
 class GraphHopper
 {
+
+    const GRAPHHOPPER_API_KEY = 'f8b0585b-1bed-4cda-aede-dfdd2c4899a9';
+
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * GraphHopper constructor.
+     */
+    public function __construct()
+    {
+        $this->client = new Client();
+    }
+
 
     public function findRound($pickup, $dropoffs)
     {
@@ -17,6 +37,46 @@ class GraphHopper
     {
         // cannot have pickup at on the pikcup
 
+    }
+
+    public function optimize($pickup, $dropoffs)
+    {
+        $url = 'https://graphhopper.com/api/1/vrp/optimize?key=' . self::GRAPHHOPPER_API_KEY;
+        $body = json_encode($this->getRequest($pickup, $dropoffs));
+
+        try {
+            $response = $this->client->request('POST', $url, [
+                'body' => $body,
+                'headers' => ['Content-Type' => 'application/json']
+            ]);
+        } catch (RequestException $e) {
+            if ($e->hasResponse()) {
+                $response = $e->getResponse();
+            } else {
+                throw $e;
+            }
+        }
+
+        return ApiResponseFactory::fromGuzzleHttpResponse($response);
+    }
+
+    public function solution($jobId)
+    {
+        $url = 'https://graphhopper.com/api/1/vrp/solution/' . $jobId . '?key=' . self::GRAPHHOPPER_API_KEY;
+
+        try {
+            $response = $this->client->request('GET', $url, [
+                'headers' => ['Content-Type' => 'application/json']
+            ]);
+        } catch (RequestException $e) {
+            if ($e->hasResponse()) {
+                $response = $e->getResponse();
+            } else {
+                throw $e;
+            }
+        }
+
+        return ApiResponseFactory::fromGuzzleHttpResponse($response);
     }
 
     public function getRequest($pickup, $dropoffs)
@@ -65,4 +125,3 @@ class GraphHopper
         return (object)array('lat' => 0.123, 'lon' => 2.1234);
     }
 }
-
