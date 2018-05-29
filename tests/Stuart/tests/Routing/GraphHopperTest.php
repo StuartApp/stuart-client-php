d3927a770831a8cf97948dfb2a42ba8cf740ea8e
Maximilien Tyc <maximilien.tyc@gmail.com>
2018-05-29 13:39:44 +0200

    add test case to compare prices

--- tests/Stuart/tests/Routing/GraphHopperTest.php
+++ tests/Stuart/tests/Routing/GraphHopperTest.php
@@ -3,6 +3,7 @@
 namespace Stuart\Tests;
 
 use Stuart\DropOff;
+use Stuart\Job;
 use Stuart\Pickup;
 use Stuart\Routing\GraphHopper;
 
@@ -16,40 +17,53 @@ class GraphHopperTest extends \PHPUnit_Framework_TestCase
 
         // when
         $dropoffs = [
-            $this->dropoff('23 rue de richelieu 75002 paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-29 12:40:00')),
-            $this->dropoff('3 rue d\'edimbourg 75008 paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-29 12:45:00')),
-            $this->dropoff('156 rue de charonne 75012 paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-29 12:30:00')),
-            $this->dropoff('8 rue sidi brahim 75012 paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-29 14:30:00')),
-            $this->dropoff('5 passage du chantier 75012 paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-29 12:30:00')),
-            $this->dropoff('Hôpital Saint-Louis, 75010 Paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-29 13:20:00')),
-            $this->dropoff('1 Rue des Deux Gares, 75010 Paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-29 12:30:00')),
-            $this->dropoff('137 Rue la Fayette, 75010 Paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-29 12:30:00')),
-            $this->dropoff('34 Rue Pierre Semard, 75009 Paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-29 12:00:00')),
-            $this->dropoff('46 Rue Lecourbe, 75015 Paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-29 12:30:00')),
-            $this->dropoff('178 Rue Lecourbe, 75015 Paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-29 13:00:00')),
-            $this->dropoff('43 Rue des Alouettes 75019 Paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-29 12:30:00')),
-            $this->dropoff('50 Rue Durantin, 75018 Paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-29 12:30:00')),
-            $this->dropoff('47-33 Rue des Abbesses, 75018 Paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-29 13:30:00')),
-            $this->dropoff('2 Boulevard de la Villette, 75019 Paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-29 14:30:00')),
-            $this->dropoff('172 Rue de Charonne, 75011 Paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-29 15:30:00'))
+            $this->dropoff('23 rue de richelieu 75002 paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-30 12:40:00')),
+            $this->dropoff('3 rue d\'edimbourg 75008 paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-30 12:45:00')),
+            $this->dropoff('156 rue de charonne 75012 paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-30 12:30:00')),
+            $this->dropoff('8 rue sidi brahim 75012 paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-30 14:30:00')),
+            $this->dropoff('5 passage du chantier 75012 paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-30 12:30:00')),
+            $this->dropoff('Hôpital Saint-Louis, 75010 Paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-30 13:20:00')),
+            $this->dropoff('1 Rue des Deux Gares, 75010 Paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-30 12:30:00')),
+            $this->dropoff('137 Rue la Fayette, 75010 Paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-30 12:30:00')),
+            $this->dropoff('34 Rue Pierre Semard, 75009 Paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-30 12:00:00')),
+            $this->dropoff('46 Rue Lecourbe, 75015 Paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-30 12:30:00')),
+            $this->dropoff('178 Rue Lecourbe, 75015 Paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-30 13:00:00')),
+            $this->dropoff('43 Rue des Alouettes 75019 Paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-30 12:30:00')),
+            $this->dropoff('50 Rue Durantin, 75018 Paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-30 12:30:00')),
+            $this->dropoff('47-33 Rue des Abbesses, 75018 Paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-30 13:30:00')),
+            $this->dropoff('2 Boulevard de la Villette, 75019 Paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-30 14:30:00')),
+            $this->dropoff('172 Rue de Charonne, 75011 Paris', \DateTime::createFromFormat('Y-m-d H:i:s', '2018-05-30 15:30:00'))
         ];
 
         $config = array(
             'graphhopper_api_key' => 'f8b0585b-1bed-4cda-aede-dfdd2c4899a9',
             'vehicle_count' => 10,
             'max_dropoffs' => 8,
-            'slot_size_in_minutes' => 30,
-            'max_distance' => 10000
+            'slot_size_in_minutes' => 60,
+            'max_distance' => 15000
         );
 
+        $pricingStacking = 0;
         $graphHopper = new GraphHopper($pickup, $dropoffs, $config);
         $result = $graphHopper->findRounds();
         foreach ($result->jobs as $job) {
             $job->setTransportType('bike');
             $res = $this->getPricing($job);
-            print_r($res);
+            $pricingStacking += $res->amount;
         }
-        print_r('Waste count: ' . count($result->waste));
+        print_r('Total pricing with stacking is: ' . $pricingStacking . ', Waste count is: ' . count($result->waste) . '. ');
+
+        $pricingNoStacking = 0;
+        foreach ($dropoffs as $dropoff) {
+            $job = new Job();
+            $job->setTransportType('bike');
+            $job->pushPickup($pickup);
+            $job->pushDropoff($dropoff);
+            $res = $this->getPricing($job);
+            $pricingNoStacking += $res->amount;
+        }
+        print_r('Total pricing without stacking is: ' . $pricingNoStacking);
+
     }
 
     private function getPricing($job)
